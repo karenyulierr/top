e8472c8a3cfa123d2060f7b9fe03cc207517e3d7
karenyulier <karenyulierr@gmail.com>
2021-03-25 21:17:31 -0500

    commit inicial

---
+++ apps.js
@@ -0,0 +1,46 @@
+/**. Cuenta los caracteres
+Escribe una función en JavaScript llamada count que reciba dos parámetros: str y char. 
+La función debe retornar el número de veces que aparece char en str. */
+
+
+// function count(str ,char) {
+//     let contador=0;
+//     for(let i=0;i<str.length;i++){
+//         if(str[i]==char){
+//            contador++;
+//         }
+//     }
+//     return contador;
+//   }
+
+//   console.log(count("holaaa",'k'));
+
+/*
+Filtra el arreglo
+Escribe una función en JavaScript llamada filter que reciba un arreglo de números
+ como parámetro y retorne un nuevo arreglo con los números mayores a 10.
+
+
+ function filter (numeros){
+    let newArray=[];
+    for(let i=0; i<numeros.length; i++){
+        if(numeros[i]>10){
+            newArray.push(numeros[i]);
+        }
+    }
+    return newArray;
+ }
+
+ console.log(filter([3, 12, 45, 7])); 
+ console.log(filter([3, 5]));   */
+
+ /*4. Calcula la hipotenusa
+Escribe una función en JavaScript llamada hypotenuse que reciba dos parámetros: a y b,
+ que representan los lados de un triángulo, y que retorne la hipotenusa.*/
+
+//  function hypotenuse (a,b){
+//     return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+//  }
+
+//  console.log(hypotenuse(3, 4));
+//  console.log(hypotenuse(10, 13));
